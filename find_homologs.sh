b1895bb81e50c6c7f283170727ee86588dc769ea
santoun6 <santoun6@gatech.edu>
2026-09-20 23:13:53 -0400

    fix: recreate find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,13 +1,5 @@
-#!/bin/bash
-
-# assignments line
-QUERY=$1
-SUBJECT=$2
-OUTPUT=$3
-
-# use blastn filter using column numbers
-tblastn -query "$QUERY" -subject "$SUBJECT" -outfmt "6 std qlen" | \
-awk '$3 > 30 && $4 >= 0.9 * $13' > "$OUTPUT"
-
-# print number of matches 
-wc -l < "$OUTPUT"
+cat << 'EOF' > find_homologs.sh
+#!/usr/bin/env bash
+tblastn -query "$1" -subject "$2" -outfmt '6 std qlen' | awk '$3 > 30 && $4 >= 0.9 * $13' > "$3"
+wc -l < "$3"
+EOF
